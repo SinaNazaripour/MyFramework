a17a1be688c8e57903246fac25ba44d215660aec
SinaNazaripour <sinanazaripour@gmail.com>
2025-10-26 15:38:10 +0330

    session id regeneration

--- src/App/Services/UserService.php
+++ src/App/Services/UserService.php
@@ -74,6 +74,7 @@ class UserService
             throw new ValidationException(["password" => ["Invalid credentials"]]);
         }
 
+        session_regenerate_id();
         $_SESSION['user'] = $user['id'];
     }
 }
