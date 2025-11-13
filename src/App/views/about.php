94a21f51485a100bf281c3eeb88376c0e069f044
Sina Nazaripour <sinanazaripour@gmail.com>
2025-11-13 22:17:39 +0330

    Update about.php

--- src/App/views/about.php
+++ src/App/views/about.php
@@ -2,7 +2,7 @@
   include $this->resolve("partials/_header.php");
   ?>
 
- <h1> <?php echo e($dangerousData) ?></h1>
+ <h1> <?php echo e($dangerousData); ?></h1>
  <?php
   include $this->resolve("partials/_footer.php")
   ?>
